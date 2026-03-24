aaaf17f9ed2605a448545a9021c62924590a35ef
nillchik <daniilzhitko22@mail.ru>
2026-03-24 16:52:12 +0300

    Initial Commit

---
+++ CoffeeBreakLanguages/CoffeeBreakLanguagesApp.swift
@@ -0,0 +1,17 @@
+//
+//  CoffeeBreakLanguagesApp.swift
+//  CoffeeBreakLanguages
+//
+//  Created by split on 24/03/2026.
+//
+
+import SwiftUI
+
+@main
+struct CoffeeBreakLanguagesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
